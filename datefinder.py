fde5c3b59549aa054912e21d4664d8b6c2f7acd4
Przemek <przemyslaw.d.brzezinski@gmail.com>
2018-12-19 11:41:16 +0100

    datefinder + dateparser

---
+++ datefinder.py
@@ -0,0 +1,352 @@
+import copy
+import logging
+import regex as re
+from dateutil import tz, parser
+
+logger = logging.getLogger('datefinder')
+
+
+class DateFinder(object):
+    """
+    Locates dates in a text
+    """
+    MONTHS_PATTERN = r"stycze[n,ń]|stycz[n,ń]i?[a,u]|sty|january|jan|lutym?|lutego|lut|february|feb|marzec|marcu|marca|mar|march|kwiecie[n,ń]|kwietni[a,u]|kwi|april|maj|maja|maju|mai|czerwiec|czerwca|czerwcu|cze|june|jun|lipiec|lipca|lipcu|lip|lipcem|july|jul|sierp[n,ń]i[a,u]|sierpie[n,ń]|sie|august|aug|wrzesie[ń,n]|wrzesie[ń,n]i[a,u]|wrz|september|sep|pa[ź,z]dziernik|pa[z,ź]dziernik[a,u]|pa[z,ź]|october|oct|listopad|listopada|listopadzie|lis|november|nov|grudnia|grudniu|grudzie,n]|gru|december|dec"
+
+    DIGITS_MODIFIER_PATTERN = '\d+st|\d+th|\d+rd|first|second|third|fourth|fifth|sixth|seventh|eighth|nineth|tenth|next|last'
+    DIGITS_PATTERN = '\d+'
+    DAYS_PATTERN = 'monday|tuesday|wednesday|thursday|friday|saturday|sunday|mon|tue|tues|wed|thur|thurs|fri|sat|sun'
+    # MONTHS_PATTERN = 'january|february|march|april|may|june|july|august|september|october|november|december|jan|feb|mar|apr|may|jun|jul|aug|sep|sept|oct|nov|dec'
+    TIMEZONES_PATTERN = 'ACDT|ACST|ACT|ACWDT|ACWST|ADDT|ADMT|ADT|AEDT|AEST|AFT|AHDT|AHST|AKDT|AKST|AKTST|AKTT|ALMST|ALMT|AMST|AMT|ANAST|ANAT|ANT|APT|AQTST|AQTT|ARST|ART|ASHST|ASHT|AST|AWDT|AWST|AWT|AZOMT|AZOST|AZOT|AZST|AZT|BAKST|BAKT|BDST|BDT|BEAT|BEAUT|BIOT|BMT|BNT|BORT|BOST|BOT|BRST|BRT|BST|BTT|BURT|CANT|CAPT|CAST|CAT|CAWT|CCT|CDDT|CDT|CEDT|CEMT|CEST|CET|CGST|CGT|CHADT|CHAST|CHDT|CHOST|CHOT|CIST|CKHST|CKT|CLST|CLT|CMT|COST|COT|CPT|CST|CUT|CVST|CVT|CWT|CXT|ChST|DACT|DAVT|DDUT|DFT|DMT|DUSST|DUST|EASST|EAST|EAT|ECT|EDDT|EDT|EEDT|EEST|EET|EGST|EGT|EHDT|EMT|EPT|EST|ET|EWT|FET|FFMT|FJST|FJT|FKST|FKT|FMT|FNST|FNT|FORT|FRUST|FRUT|GALT|GAMT|GBGT|GEST|GET|GFT|GHST|GILT|GIT|GMT|GST|GYT|HAA|HAC|HADT|HAE|HAP|HAR|HAST|HAT|HAY|HDT|HKST|HKT|HLV|HMT|HNA|HNC|HNE|HNP|HNR|HNT|HNY|HOVST|HOVT|HST|ICT|IDDT|IDT|IHST|IMT|IOT|IRDT|IRKST|IRKT|IRST|ISST|IST|JAVT|JCST|JDT|JMT|JST|JWST|KART|KDT|KGST|KGT|KIZST|KIZT|KMT|KOST|KRAST|KRAT|KST|KUYST|KUYT|KWAT|LHDT|LHST|LINT|LKT|LMT|LMT|LMT|LMT|LRT|LST|MADMT|MADST|MADT|MAGST|MAGT|MALST|MALT|MART|MAWT|MDDT|MDST|MDT|MEST|MET|MHT|MIST|MIT|MMT|MOST|MOT|MPT|MSD|MSK|MSM|MST|MUST|MUT|MVT|MWT|MYT|NCST|NCT|NDDT|NDT|NEGT|NEST|NET|NFT|NMT|NOVST|NOVT|NPT|NRT|NST|NT|NUT|NWT|NZDT|NZMT|NZST|OMSST|OMST|ORAST|ORAT|PDDT|PDT|PEST|PET|PETST|PETT|PGT|PHOT|PHST|PHT|PKST|PKT|PLMT|PMDT|PMMT|PMST|PMT|PNT|PONT|PPMT|PPT|PST|PT|PWT|PYST|PYT|QMT|QYZST|QYZT|RET|RMT|ROTT|SAKST|SAKT|SAMT|SAST|SBT|SCT|SDMT|SDT|SET|SGT|SHEST|SHET|SJMT|SLT|SMT|SRET|SRT|SST|STAT|SVEST|SVET|SWAT|SYOT|TAHT|TASST|TAST|TBIST|TBIT|TBMT|TFT|THA|TJT|TKT|TLT|TMT|TOST|TOT|TRST|TRT|TSAT|TVT|ULAST|ULAT|URAST|URAT|UTC|UYHST|UYST|UYT|UZST|UZT|VET|VLAST|VLAT|VOLST|VOLT|VOST|VUST|VUT|WARST|WART|WAST|WAT|WDT|WEDT|WEMT|WEST|WET|WFT|WGST|WGT|WIB|WIT|WITA|WMT|WSDT|WSST|WST|WT|XJT|YAKST|YAKT|YAPT|YDDT|YDT|YEKST|YEKST|YEKT|YEKT|YERST|YERT|YPT|YST|YWT|zzz'
+    ## explicit north american timezones that get replaced
+    NA_TIMEZONES_PATTERN = 'pacific|eastern|mountain|central'
+    ALL_TIMEZONES_PATTERN = TIMEZONES_PATTERN + '|' + NA_TIMEZONES_PATTERN
+    DELIMITERS_PATTERN = '[/\:\-\,\s\_\+\@\.]+'
+    TIME_PERIOD_PATTERN = 'a\.m\.|am|p\.m\.|pm'
+    ## can be in date strings but not recognized by dateutils
+    EXTRA_TOKENS_PATTERN = 'due|by|on|standard|daylight|savings|time|date|of|to|until|z|at|t'
+
+    ## TODO: Get english numbers?
+    ## http://www.rexegg.com/regex-trick-numbers-in-english.html
+
+    RELATIVE_PATTERN = 'before|after|next|last|ago'
+    TIME_SHORTHAND_PATTERN = 'noon|midnight|today|yesterday'
+    UNIT_PATTERN = 'second|minute|hour|day|week|month|year'
+
+    ## Time pattern is used independently, so specified here.
+    TIME_PATTERN = """
+    (?P<time>
+        ## Captures in format XX:YY(:ZZ) (PM) (EST)
+        (
+            (?P<hours>\d{{1,2}})
+            \:
+            (?P<minutes>\d{{1,2}})
+            (\:(?<seconds>\d{{1,2}}))?
+            ([\.\,](?<microseconds>\d{{1,6}}))?
+            \s*
+            (?P<time_periods>{time_periods})?
+            \s*
+            (?P<timezones>{timezones})?
+        )
+        |
+        ## Captures in format 11 AM (EST)
+        ## Note with single digit capture requires time period
+        (
+            (?P<hours>\d{{1,2}})
+            \s*
+            (?P<time_periods>{time_periods})
+            \s*
+            (?P<timezones>{timezones})*
+        )
+    )
+    """.format(
+        time_periods=TIME_PERIOD_PATTERN,
+        timezones=ALL_TIMEZONES_PATTERN
+    )
+
+    DATES_PATTERN = """
+    (
+        (
+            {time}
+            |
+            ## Grab any digits
+            (?P<digits_modifier>{digits_modifier})
+            |
+            (?P<digits>{digits})
+            |
+            (?P<days>{days})
+            |
+            (?P<months>{months})
+            |
+            ## Delimiters, ie Tuesday[,] July 18 or 6[/]17[/]2008
+            ## as well as whitespace
+            (?P<delimiters>{delimiters})
+            |
+            ## These tokens could be in phrases that dateutil does not yet recognize
+            ## Some are US Centric
+            (?P<extra_tokens>{extra_tokens})
+        ## We need at least three items to match for minimal datetime parsing
+        ## ie 10pm
+        ){{3,}}
+    )
+    """
+
+    DATES_PATTERN = DATES_PATTERN.format(
+        time=TIME_PATTERN,
+        digits=DIGITS_PATTERN,
+        digits_modifier=DIGITS_MODIFIER_PATTERN,
+        days=DAYS_PATTERN,
+        months=MONTHS_PATTERN,
+        delimiters=DELIMITERS_PATTERN,
+        extra_tokens=EXTRA_TOKENS_PATTERN
+    )
+
+    DATE_REGEX = re.compile(DATES_PATTERN, re.IGNORECASE | re.MULTILINE | re.UNICODE | re.DOTALL | re.VERBOSE)
+
+    TIME_REGEX = re.compile(TIME_PATTERN, re.IGNORECASE | re.MULTILINE | re.UNICODE | re.DOTALL | re.VERBOSE)
+
+    ## These tokens can be in original text but dateutil
+    ## won't handle them without modification
+    REPLACEMENTS = {
+        "standard": " ",
+        "daylight": " ",
+        "savings": " ",
+        "time": " ",
+        "date": " ",
+        "by": " ",
+        "due": " ",
+        "on": " ",
+        "to": " ",
+    }
+
+    TIMEZONE_REPLACEMENTS = {
+        "pacific": "PST",
+        "eastern": "EST",
+        "mountain": "MST",
+        "central": "CST",
+    }
+
+    ## Characters that can be removed from ends of matched strings
+    STRIP_CHARS = ' \n\t:-.,_'
+
+    def __init__(self, base_date=None):
+        self.base_date = base_date
+
+    def find_dates(self, text, source=False, index=False, strict=False):
+
+        for date_string, indices in self.extract_date_strings(text, strict=strict):
+
+            # as_dt = self.parse_date_string(date_string, captures)
+            # if as_dt is None:
+            #     ## Dateutil couldn't make heads or tails of it
+            #     ## move on to next
+            #     continue
+            #
+            # returnables = (as_dt,)
+            # if source:
+            returnables = (date_string,)
+            # if index:
+            returnables = returnables + (indices,)
+
+            if len(returnables) == 1:
+                returnables = returnables[0]
+            yield returnables
+
+    def _find_and_replace(self, date_string, captures):
+        """
+        :warning: when multiple tz matches exist the last sorted capture will trump
+        :param date_string:
+        :return: date_string, tz_string
+        """
+        # add timezones to replace
+        cloned_replacements = copy.copy(self.REPLACEMENTS)  # don't mutate
+        for tz_string in captures.get('timezones', []):
+            cloned_replacements.update({tz_string: ' '})
+
+        date_string = date_string.lower()
+        for key, replacement in cloned_replacements.items():
+            # we really want to match all permutations of the key surrounded by whitespace chars except one
+            # for example: consider the key = 'to'
+            # 1. match 'to '
+            # 2. match ' to'
+            # 3. match ' to '
+            # but never match r'(\s|)to(\s|)' which would make 'october' > 'ocber'
+            date_string = re.sub(r'(^|\s)' + key + '(\s|$)', replacement, date_string, flags=re.IGNORECASE)
+
+        return date_string, self._pop_tz_string(sorted(captures.get('timezones', [])))
+
+    def _pop_tz_string(self, list_of_timezones):
+        try:
+            tz_string = list_of_timezones.pop()
+            # make sure it's not a timezone we
+            # want replaced with better abbreviation
+            return self.TIMEZONE_REPLACEMENTS.get(tz_string, tz_string)
+        except IndexError:
+            return ''
+
+    def _add_tzinfo(self, datetime_obj, tz_string):
+        """
+        take a naive datetime and add dateutil.tz.tzinfo object
+
+        :param datetime_obj: naive datetime object
+        :return: datetime object with tzinfo
+        """
+        if datetime_obj is None:
+            return None
+
+        tzinfo_match = tz.gettz(tz_string)
+        return datetime_obj.replace(tzinfo=tzinfo_match)
+
+    def parse_date_string(self, date_string, captures):
+        # For well formatted string, we can already let dateutils parse them
+        # otherwise self._find_and_replace method might corrupt them
+        try:
+            as_dt = parser.parse(date_string, default=self.base_date)
+        except ValueError:
+            # replace tokens that are problematic for dateutil
+            date_string, tz_string = self._find_and_replace(date_string, captures)
+
+            ## One last sweep after removing
+            date_string = date_string.strip(self.STRIP_CHARS)
+            ## Match strings must be at least 3 characters long
+            ## < 3 tends to be garbage
+            if len(date_string) < 3:
+                return None
+
+            try:
+                logger.debug('Parsing {0} with dateutil'.format(date_string))
+                as_dt = parser.parse(date_string, default=self.base_date)
+            except Exception as e:
+                logger.debug(e)
+                as_dt = None
+            if tz_string:
+                as_dt = self._add_tzinfo(as_dt, tz_string)
+        return as_dt
+
+    def extract_date_strings(self, text, strict=False):
+        """
+        Scans text for possible datetime strings and extracts them
+
+        source: also return the original date string
+        index: also return the indices of the date string in the text
+        strict: Strict mode will only return dates sourced with day, month, and year
+        """
+        results = []
+        for match in self.DATE_REGEX.finditer(text):
+
+            match_str = match.group(0)
+            indices = match.span(0)
+
+            # ## Get individual group matches
+            # captures = match.capturesdict()
+            # time = captures.get('time')
+            # digits = captures.get('digits')
+            # digits_modifiers = captures.get('digits_modifiers')
+            # days = captures.get('days')
+            # months = captures.get('months')
+            # timezones = captures.get('timezones')
+            # delimiters = captures.get('delimiters')
+            # time_periods = captures.get('time_periods')
+            # extra_tokens = captures.get('extra_tokens')
+            #
+            # if strict:
+            #     complete = False
+            #     ## 12-05-2015
+            #     if len(digits) == 3:
+            #         complete = True
+            #     ## 19 February 2013 year 09:10
+            #     elif (len(months) == 1) and (len(digits) == 2):
+            #         complete = True
+            #
+            #     if not complete:
+            #         continue
+
+            ## sanitize date string
+            ## replace unhelpful whitespace characters with single whitespace
+
+            strings_and_indexes = self.get_splitted_strings_with_indexes('[\n\t]+', match_str, indices)
+            if (len(strings_and_indexes) > 0):
+                strings_and_indexes = [(re.sub('[\n\t\s\xa0]+', ' ', s[0]), s[1]) for s in strings_and_indexes]
+                strings_and_indexes = [(s[0].strip(self.STRIP_CHARS), s[1]) for s in strings_and_indexes]
+                results.extend(strings_and_indexes)
+                continue
+
+            match_str = re.sub('[\n\t\s\xa0]+', ' ', match_str)
+            match_str = match_str.strip(self.STRIP_CHARS)
+
+            ## Save sanitized source string
+            results.append((match_str, indices))
+        return results
+
+    def get_split_sentences_indexes(self, regex, string):
+        indexes = []
+        size = len(string)
+        if not regex:
+            indexes.append((0, size - 1))
+            return indexes
+        brake_indexes = [v.span() for v in re.finditer(regex, string)]
+        if brake_indexes is None or len(brake_indexes) == 0:
+            indexes.append((0, size - 1))
+            return indexes
+        brake_index = 0
+        current_start = 0
+        current_index = 0
+        while current_index < size:
+            if brake_index == len(brake_indexes):
+                indexes.append((current_start, size - 1))
+                break
+            brake_start, brake_stop = brake_indexes[brake_index]
+            if brake_start == 0:
+                current_start = brake_stop
+                current_index = brake_stop
+                brake_index += 1
+                continue
+            if current_index == brake_start:
+                brake_index += 1
+                indexes.append((current_start, current_index - 1))
+                current_start = brake_stop
+                current_index = brake_stop - 1
+            current_index += 1
+        return indexes
+
+    def get_splitted_strings_with_indexes(self, regex, string, indices):
+        indexes = [(indices[0] + k[0], indices[0] + k[1]) for k in self.get_split_sentences_indexes(regex, string)]
+        splitted = [r for r in re.split(regex, string) if r]
+        if len(indexes) != len(splitted):
+            raise RuntimeError
+        return list(zip(splitted, indexes))
+
+
+def find_dates(
+    text,
+    source=False,
+    index=False,
+    strict=False,
+    base_date=None
+):
+    """
+    Extract datetime strings from text
+
+    :param text:
+        A string that contains one or more natural language or literal
+        datetime strings
+    :type text: str|unicode
+    :param source:
+        Return the original string segment
+    :type source: boolean
+    :param index:
+        Return the indices where the datetime string was located in text
+    :type index: boolean
+    :param strict:
+        Only return datetimes with complete date information. For example:
+        `July 2016` of `Monday` will not return datetimes.
+        `May 16, 2015` will return datetimes.
+    :type strict: boolean
+    :param base_date:
+        Set a default base datetime when parsing incomplete dates
+    :type base_date: datetime
+
+    :return: Returns a generator that produces :mod:`datetime.datetime` objects,
+        or a tuple with the source text and index, if requested
+    """
+    date_finder = DateFinder(base_date=base_date)
+    return date_finder.find_dates(text, source=source, index=index, strict=strict)
--- main.py
+++ main.py
@@ -141,89 +141,8 @@ import dateparser.search as search
 # print(x)
 # print(y)
 
-import regex as re
 
-splitters = r"(-|\/|\\|\s|,|\.)"
-months = r"(0?[1-9]|10|11|12)"
-days = r"(0?[1-9]|[1-2][0-9]|3[0-1])"
-years = r"((19)([2-9])(\d{1})|(20)([01])(\d{1})|([8901])(\d{1}))"
-mdy_pattern = "%(months)s%(splitters)s%(days)s%(splitters)s%(years)s" % {'splitters': splitters, 'months': months,
-                                                                         'days': days, 'years': years}
-dmy_pattern = "%(days)s%(splitters)s%(months)s%(splitters)s%(years)s" % {'splitters': splitters, 'months': months,
-                                                                         'days': days, 'years': years}
-ymd_pattern = "%(years)s%(splitters)s%(months)s%(splitters)s%(days)s" % {'splitters': splitters, 'months': months,
-                                                                         'days': days, 'years': years}
-ydm_pattern = "%(years)s%(splitters)s%(days)s%(splitters)s%(months)s" % {'splitters': splitters, 'months': months,
-                                                                         'days': days, 'years': years}
-my_pattern = "%(months)s%(splitters)s%(years)s" % {'splitters': splitters, 'months': months, 'years': years}
-ym_pattern = "%(years)s%(splitters)s%(months)s" % {'splitters': splitters, 'months': months, 'years': years}
-dy_pattern = "%(days)s%(splitters)s%(years)s" % {'splitters': splitters, 'days': days, 'years': years}
-yd_pattern = "%(years)s%(splitters)s%(days)s" % {'splitters': splitters, 'days': days, 'years': years}
-
-dm_pattern = "%(days)s%(splitters)s%(months)s" % {'splitters': splitters, 'days': days, 'months': months}
-md_pattern = "%(months)s%(splitters)s%(days)s" % {'splitters': splitters, 'days': days, 'months': months}
-
-january = r"stycze[n,ń]|stycz[n,ń]i?[a,u]|sty|january|jan"
-february = r"lutym?|lutego|lut|february|feb"
-march = r"marzec|marcu|marca|mar|march"
-april = r"kwiecie[n,ń]|kwietni[a,u]|kwi|april"
-may = r"maj|maja|maju|mai"
-june = r"czerwiec|czerwca|czerwcu|cze|june|jun"
-july = r"lipiec|lipca|lipcu|lip|lipcem|july|jul"
-august = r"sierp[n,ń]i[a,u]|sierpie[n,ń]|sie|august|aug"
-september = r"wrzesie[ń,n]|wrzesie[ń,n]i[a,u]|wrz|september|sep"
-october = r"pa[ź,z]dziernik|pa[z,ź]dziernik[a,u]|pa[z,ź]|october|oct"
-november = r"listopad|listopada|listopadzie|lis|november|nov"
-december = r"grudnia|grudniu|grudzie,n]|gru|december|dec"
-
-all_months = r"(" + january + "|" + february + "|" + march + "|" + april + "|" + may + "|" + june + "|" + july + "|" + august + "|" + september + "|" + october + "|" + november + "|" + december + ")"
-
-mdy_pattern_literal = "%(months)s%(splitters)s%(days)s%(splitters)s%(years)s" % {'splitters': splitters,
-                                                                                 'months': all_months,
-                                                                                 'days': days, 'years': years}
-dmy_pattern_literal = "%(days)s%(splitters)s%(months)s%(splitters)s%(years)s" % {'splitters': splitters,
-                                                                                 'months': all_months,
-                                                                                 'days': days, 'years': years}
-ymd_pattern_literal = "%(years)s%(splitters)s%(months)s%(splitters)s%(days)s" % {'splitters': splitters,
-                                                                                 'months': all_months,
-                                                                                 'days': days, 'years': years}
-ydm_pattern_literal = "%(years)s%(splitters)s%(days)s%(splitters)s%(months)s" % {'splitters': splitters,
-                                                                                 'months': all_months,
-                                                                                 'days': days, 'years': years}
-my_pattern_literal = "%(months)s%(splitters)s%(years)s" % {'splitters': splitters, 'months': all_months, 'years': years}
-ym_pattern_literal = "%(years)s%(splitters)s%(months)s" % {'splitters': splitters, 'months': all_months, 'years': years}
-dy_pattern_literal = "%(days)s%(splitters)s%(years)s" % {'splitters': splitters, 'days': days, 'years': years}
-yd_pattern_literal = "%(years)s%(splitters)s%(days)s" % {'splitters': splitters, 'days': days, 'years': years}
-
-dm_pattern_literal = "%(days)s%(splitters)s%(months)s" % {'splitters': splitters, 'days': days, 'months': all_months}
-md_pattern_literal = "%(months)s%(splitters)s%(days)s" % {'splitters': splitters, 'days': days, 'months': all_months}
-
-all_patterns = []
-dmy_com = re.compile(dmy_pattern, flags=re.IGNORECASE)
-mdy_com = re.compile(mdy_pattern, flags=re.IGNORECASE)
-ydm_com = re.compile(ydm_pattern, flags=re.IGNORECASE)
-ymd_com = re.compile(ymd_pattern, flags=re.IGNORECASE)
-dy_com = re.compile(dy_pattern, flags=re.IGNORECASE)
-my_com = re.compile(my_pattern, flags=re.IGNORECASE)
-dmy_literal_com = re.compile(dmy_pattern_literal, flags=re.IGNORECASE)
-mdy_literal_com = re.compile(mdy_pattern_literal, flags=re.IGNORECASE)
-ydm_literal_com = re.compile(ydm_pattern_literal, flags=re.IGNORECASE)
-ymd_literal_com = re.compile(ymd_pattern_literal, flags=re.IGNORECASE)
-dy_literal_com = re.compile(dy_pattern_literal, flags=re.IGNORECASE)
-my_literal_com = re.compile(my_pattern_literal, flags=re.IGNORECASE)
-all_patterns.append(dmy_com)
-all_patterns.append(mdy_com)
-all_patterns.append(ydm_com)
-all_patterns.append(ymd_com)
-all_patterns.append(dy_com)
-all_patterns.append(my_com)
-all_patterns.append(dmy_literal_com)
-all_patterns.append(mdy_literal_com)
-all_patterns.append(ydm_literal_com)
-all_patterns.append(ymd_literal_com)
-all_patterns.append(dy_literal_com)
-all_patterns.append(my_literal_com)
-
+import datefinder
 
 def remove_duplicates(substring_list):
     used_spans = []
@@ -239,27 +158,26 @@ def remove_duplicates(substring_list):
 def validate(substring_list):
     indexes_to_remove = []
     sorted_list = sorted(remove_duplicates(substring_list), key=lambda e: e[1][0])
-    for i in range(len(sorted_list) - 1):
-        first = sorted_list[i]
-        first_span = first[1]
-        next = sorted_list[i + 1]
-        next_span = next[1]
-        if first_span[0] == next_span[0] and first_span[1] < next_span[1]:
-            indexes_to_remove.append(i)
-        if first_span[0] == next_span[0] and first_span[1] > next_span[1]:
-            indexes_to_remove.append(i + 1)
-        if first_span[0] < next_span[0] and first_span[1] == next_span[1]:
-            indexes_to_remove.append(i + 1)
-    for i in reversed(indexes_to_remove):
+    for i in range(len(sorted_list)):
+        for j in range(i, len(sorted_list)):
+            first = sorted_list[i]
+            first_span = first[1]
+            next = sorted_list[j]
+            next_span = next[1]
+            if first_span[0] == next_span[0] and first_span[1] < next_span[1]:
+                indexes_to_remove.append(i)
+            if first_span[0] <= next_span[0] and first_span[1] > next_span[1]:
+                indexes_to_remove.append(j)
+            if first_span[0] < next_span[0] and first_span[1] == next_span[1]:
+                indexes_to_remove.append(j)
+    for i in reversed(list(set(indexes_to_remove))):
         sorted_list.remove(sorted_list[i])
     return sorted_list
 
 
 def find_dates_substring(text):
-    found_substrings = []
-    for pattern in all_patterns:
-        found_substrings.extend([(r.group(0), r.span()) for r in re.finditer(pattern, text)])
-    return validate(found_substrings)
+    dates = datefinder.find_dates(text,source= True, index=True)
+    return [(k,v) for k,v in dates]
 
 
 import dateparser
@@ -269,25 +187,88 @@ import dateparser.search as search
 def find_dates(text):
     substrings = find_dates_substring(text)
     dates = []
-    for s in substrings:
-        substring = s[0]
+    for s,i in substrings:
+        substring = s
         parsed_date = dateparser.parse(substring, languages=["pl"])
         if parsed_date is None:
             parsed_date = dateparser.parse(substring, languages=["en"])
             if parsed_date is None:
+                search_results = search.search_dates(substring, languages=["pl"])
+                if search_results is None:
+                    continue
                 search_results = [x[1] for x in search.search_dates(substring, languages=["pl"])]
                 if len(search_results) == 1:
-                    dates.append((s[1], search_results[0]))
+                    dates.append((i, search_results[0]))
                 elif len(search_results) > 1:
-                    dates.extend([(s[1], x[1]) for x in search_results])
-            dates.append((s[0], parsed_date))
-        dates.append((s[1], parsed_date))
-    return dates
+                    dates.extend([(i, x[1]) for x in search_results])
+            else:
+                dates.append((i, parsed_date))
+        else:
+            dates.append((i, parsed_date))
+    return [(k, str(v)) for k, v in dates]
 
-nazwa_pliku = 'strona_z_internetu_3'
+
+j = '/home/przemek/Desktop/pracai/dane/nazwyPlikówIOczekiwane.json'
+with open(j) as f:
+    nazwyPlikowIOczekiwane = f.read()
+
+import json
+
+nazwa_pliku = 'youtube_1'
 nazwa_pliku_z_format = nazwa_pliku + '.txt'
 fname = '/home/przemek/Desktop/pracai/dane/' + nazwa_pliku_z_format
+
+js = json.loads(nazwyPlikowIOczekiwane)
+
+e = next(filter(lambda x: x['name'] == nazwa_pliku_z_format, js['files']), None)
+
+print(e['expected'])
+
+
+def sprawdz(excpected, results, content):
+    same_daty = sorted(results, key=lambda x: x[1])
+    v = ''
+    excpected.sort()
+    for e in excpected:
+        cr = ''
+        for r in same_daty:
+            if r[1] == e:
+                cr = r[1] + ' ' + str(r[0])
+                same_daty.remove(r)
+        v += e
+        v += '   '
+        v += cr
+        v += '\n'
+    if same_daty: v += '----NIEZNALEZIONE------\n'
+    for r in same_daty:
+        beg = r[0][0]
+        end = r[0][1]
+        v += content[beg:end+1] + '     '
+        v += r[1] + ' ' + str(r[0])
+        v += '\n'
+    return v
+
+import time
+
+
+
 with open(fname) as f:
     content = f.read()
-    for f in find_dates(content):
-        print(f)
+    start = time.time()
+    d = find_dates(content)
+    end = time.time()
+    print("Zakończyłem szukanie dat")
+    oczekiwane = e['expected']
+    r = sprawdz(oczekiwane, d, content)
+    print(r)
+    print("Czas wykonywania")
+    print(str(end - start).replace('.',','))
+    print("Ilosc znakow")
+    print(len(content))
+    print("Ile bylo data")
+    print(len(oczekiwane))
+    print("Ile zwrocil")
+    print(len(d))
+
+#
+
